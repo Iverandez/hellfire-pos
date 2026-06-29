274058166f6812ce35f64fa5da412dafd45d1a7d
Ivan Mendez <cfmotosatelite0205@gmail.com>
2026-06-29 13:31:05 -0600

    Supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://uuqfzjycgxxatntagqpb.supabase.co/rest/v1/'
+const supabaseUrl = 'https://uuqfzjycgxxatntagqpb.supabase.co'
 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1cWZ6anljZ3h4YXRudGFncXBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MTEyNjcsImV4cCI6MjA5NTM4NzI2N30.p4Mou5tAgXkCVU1QdcLhax5VjkJbM73CmKEv-VMU6gY'
 
-export const supabase = 
-createClient(  supabaseUrl,supabaseKey)
+export const supabase = createClient(  supabaseUrl,supabaseKey)
