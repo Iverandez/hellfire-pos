d7e5609f57b70b054ff1e294543d6a046b1df4da
Ivan Mendez <cfmotosatelite0205@gmail.com>
2026-08-14 14:10:42 -0600

    Hellfire

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://uuqfzjycgxxatntagqpb.supabase.co'
+const supabaseUrl =
+  import.meta.env.VITE_SUPABASE_URL
 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1cWZ6anljZ3h4YXRudGFncXBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MTEyNjcsImV4cCI6MjA5NTM4NzI2N30.p4Mou5tAgXkCVU1QdcLhax5VjkJbM73CmKEv-VMU6gY'
+const supabaseKey =
+  import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY
 
-export const supabase = createClient(  supabaseUrl,supabaseKey)
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
