6752031fb951e9f6891c3aba66f7c4446f8243cc
Ivan Mendez <cfmotosatelite0205@gmail.com>
2026-08-14 13:49:48 -0600

    Hellfire

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ndbgblpsdtonlmvxqfqq.supabase.co'
+const supabaseUrl = 'https://uuqfzjycgxxatntagqpb.supabase.co'
 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1cWZ6anljZ3h4YXRudGFncXBiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MTEyNjcsImV4cCI6MjA5NTM4NzI2N30.p4Mou5tAgXkCVU1QdcLhax5VjkJbM73CmKEv-VMU6gY'
 
